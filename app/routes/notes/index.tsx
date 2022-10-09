9575e9c2f7710b4bc67be194963982194d67691f
Dave O’Brien <me@daveobrien.co.uk>
2022-10-09 22:41:33 +0100

    feat: Integrated notes

--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -1,72 +1,24 @@
-import { Link } from '@remix-run/react'
+import { Link, useLoaderData } from '@remix-run/react'
+import { json } from '@remix-run/node'
 
-type TagProps = {
-  name: string
+import { getAllNoteListItems } from '~/models/note.server'
+
+export async function loader() {
+  const noteListItems = await getAllNoteListItems()
+  return json({ noteListItems })
 }
-
-type NoteProps = {
-  name: string
-  to: string
-  tags?: TagProps[]
-}
-
-const notes: NoteProps[] = [
-  {
-    name: 'Test not working with useEffect',
-    to: '/notes/foo',
-    tags: [{ name: 'FooTag' }],
-  },
-  {
-    name: 'YALC not working / updating',
-    to: '/notes/foo',
-    tags: [{ name: 'FooTag' }],
-  },
-  {
-    name: 'Circular JSON structure',
-    to: '/notes/foo',
-    tags: [{ name: 'FooTag' }],
-  },
-  {
-    name: 'TypeScript: Require param is another param is present',
-    to: '/notes/foo',
-    tags: [{ name: 'FooTag' }],
-  },
-  {
-    name: 'Publish alpha package',
-    to: '/notes/foo',
-    tags: [{ name: 'FooTag' }],
-  },
-  {
-    name: 'Considerations web projects',
-    to: '/notes/foo',
-    tags: [{ name: 'FooTag' }],
-  },
-  {
-    name: 'Debugging on the server',
-    to: '/notes/foo',
-    tags: [{ name: 'FooTag' }],
-  },
-  {
-    name: 'No Xcode or CLT version detected',
-    to: '/notes/foo',
-    tags: [{ name: 'FooTag' }],
-  },
-  {
-    name: 'Setting up linting and formatting in your Project',
-    to: '/notes/foo',
-    tags: [{ name: 'FooTag' }],
-  },
-]
 
 export default function NotesIndexRoute() {
+  const { noteListItems } = useLoaderData<typeof loader>()
+
   return (
     <div>
       <h2>Notes from the code face</h2>
       <ol>
-        {notes.map((note) => (
-          <li key={note.name}>
+        {noteListItems.map((note) => (
+          <li key={note.title}>
             <h2>
-              <Link to={note.to}>{note.name}</Link>
+              <Link to={note.slug}>{note.title}</Link>
             </h2>
           </li>
         ))}
